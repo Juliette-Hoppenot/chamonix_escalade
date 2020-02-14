5987b02f6f0bd3467230bc078cf34e20c9b05863
Juliette Hoppenot <juliette.hoppenot@gmail.com>
2020-02-14 19:21:23 +0100

    created 3 offers pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,15 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:home, :license, :public, :business]
 
   def home
+  end
+
+  def license
+  end
+
+  def business
+  end
+
+  def public
   end
 end
